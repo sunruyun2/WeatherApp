3ae7b7067b4120eeda93598e06c25b19641816cf
donut <sunruyun2@gmail.com>
2023-01-30 21:08:45 +0000

    URL retrieved

--- app/src/main/java/com/example/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/myweatherapp/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     String city = "cork,ie";
     String API = "9a2df53db9e19df8f2ff4eb0cc00a36c";
     ProgressBar bar;
     RelativeLayout mainContainer;
+    TextView errorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,17 @@ public class MainActivity extends AppCompatActivity {
     private class WeatherTask extends AsyncTask<String, Void, String>{
         @Override
         protected String doInBackground(String... strings) {
-            return null;
+            //fill the data with api
+            String response;
+            try {
+                //hardcode cork address and API key
+                response = new URL("https://api.openweathermap.org/data/2.5/weather?lat=51.89&lon=8.47.99&appid=9a2df53db9e19df8f2ff4eb0cc00a36c")
+                        .toString();
+
+            } catch (Exception e){
+                response = null;
+            }
+            return response;
         }
 
 
@@ -38,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
 
              mainContainer =  findViewById(R.id.mainContainer);
              mainContainer.setGravity(View.GONE);
+
+             errorText = findViewById(R.id.errorText);
+             errorText.setVisibility(View.VISIBLE);
         }
     }
 }
