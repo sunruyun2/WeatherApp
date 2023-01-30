87db2b5ad3036a0ec2cf4b9bc7cd82248bb062b9
donut <sunruyun2@gmail.com>
2023-01-30 20:53:00 +0000

    inflate layout, mainActivity (Kotlin to Java)

--- app/src/main/java/com/example/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/myweatherapp/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.myweatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    String city = "cork,ie";
+    String API = "9a2df53db9e19df8f2ff4eb0cc00a36c";
+    ProgressBar bar;
+    RelativeLayout mainContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new WeatherTask().execute();
+    }
+
+    private class WeatherTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            //set progress bar visible before load data
+            super.onPreExecute();
+             bar = findViewById(R.id.loader);
+             bar.setVisibility(View.VISIBLE);
+
+             mainContainer =  findViewById(R.id.mainContainer);
+             mainContainer.setGravity(View.GONE);
+        }
     }
 }
